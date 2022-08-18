53e17957e25dde591e9fa6180391ab2c5a1a1d8d
iamdormouse <109101481+iamdormouse@users.noreply.github.com>
2022-08-18 23:29:53 +0200

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,15 +2,14 @@ from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
-    header_xpath = "//*[text()='Scouts Panel']"
+    header_xpath = "//h5"
     login_placeholder_xpath = "//*[@id='login-label']"
     login_field_xpath = "//*[@id='login']"
     password_placeholder_xpath = "//*[@id='password-label']"
     password_field_xpath = "//*[@id='password']"
-    remind_password_hyperlink_xpath = "//a[text()='Remind password']"
-    language_button_xpath = "//*[@role='button']"
-    sign_in_button_xpath = "//*[text()='Sign in']"
+    remind_password_hyperlink_xpath = "//a"
+    language_button_xpath = "//input[@value='en' or @value='pl']"
+    sign_in_button_xpath = "//*/button[@type='submit']"
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
-        
